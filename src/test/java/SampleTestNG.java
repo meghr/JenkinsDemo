0bb3181dc980919e8ce8ef842b604f4c4e5ee7b7
meghr <meghrajsharma.skit@gmail.com>
2019-08-04 16:23:34 +0530

    Fixing the bug

--- src/test/java/SampleTestNG.java
+++ src/test/java/SampleTestNG.java
@@ -3,9 +3,9 @@ import org.testng.annotations.Test;
 
 public class SampleTestNG {
    @Test
-   public void verifyPrimeNumber(){
-       int a =5;
+   public void verifyPrimeNumber() {
+       int a = 5;
        boolean input = PrimeNumber.isPrime(a);
-       Assert.assertTrue(input,"given input is not primer");
-   
+       Assert.assertTrue(input, "given input is not primer");
+   }
 }
