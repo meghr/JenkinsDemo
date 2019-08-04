c8d6cff0d02af3f7df2bd6d5ca690cf6b89f3bf4
meghr <meghrajsharma.skit@gmail.com>
2019-08-04 15:25:41 +0530

    First Commit:Adding project with one unit test case

---
+++ src/main/java/Mathematics.java
@@ -0,0 +1,5 @@
+
+public class Mathematics {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/SampleTestNG.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTestNG {
+   @Test
+   public void verifyPrimeNumber(){
+       int a =5;
+       boolean input = PrimeNumber.isPrime(a);
+       Assert.assertTrue(input,"given input is not primer");
+   }
+}
